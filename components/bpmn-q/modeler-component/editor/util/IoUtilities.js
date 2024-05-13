e8e62caa43d1833e4506e0252b74c5d6bd73306f
nziebura <nikolaus.ziebura@stoneone.de>
2024-05-13 21:53:12 +0200

    add svg-image to update-event for preview functionality, do not display properties-group, bump version to 1.3.21

--- components/bpmn-q/modeler-component/editor/util/IoUtilities.js
+++ components/bpmn-q/modeler-component/editor/util/IoUtilities.js
@@ -61,6 +61,8 @@ export async function saveXmlAsLocalFile(
  *
  * @param modeler The bpmn modeler the bpmn diagram is opened in.
  * @param fileName The name of the file.
+ * @param fileFormat hmmmmmmmmm
+ * @param openWindow hmmmmmmmmm
  * @returns {Promise<void>}
  */
 export async function saveModelerAsLocalFile(
@@ -93,26 +95,32 @@ export async function saveModelerAsLocalFile(
 
 export async function dispatchWorkflowChangedEvent(modeler) {
   const xml = await getXml(modeler);
+  const svg = await getWorkflowAsSVG(modeler);
 
   dispatchWorkflowEvent(
       "workflow-changed",
       xml,
+      svg.svg,
       editorConfig.getFileName()
   );
 }
 
-export async function dispatchWorkflowTransformedEvent(xmlTransformed) {
+export async function dispatchWorkflowTransformedEvent(modeler, xmlTransformed) {
+  const svg = await getWorkflowAsSVG(modeler);
   dispatchWorkflowEvent(
       "workflow-transformed",
       xmlTransformed,
+      svg.svg,
       editorConfig.getFileName()
   );
 }
+
 
 /**
  * Simple Getter which returns the opened bpmn diagram of the given bpmn modeler as a xml diagram.
  *
  * @param modeler The bpmn modeler the bpmn diagram is opened in.
+ * @param format hmmmmmm
  * @returns {Promise<*>} The xml diagram.
  */
 export async function getXml(modeler, format = true) {
@@ -344,7 +352,7 @@ export function setAutoSaveInterval(
   autoSaveFileOption = editorConfig.getAutoSaveFileOption()
 ) {
   if (autoSaveFileOption === autoSaveFile.DISABLED) {
-    return;
+    // do nothing
   } else if (autoSaveFileOption === autoSaveFile.INTERVAL) {
     getModeler().autosaveIntervalId = setInterval(() => {
       saveFile();
@@ -422,6 +430,10 @@ export async function saveXmlAndViewsAsZip(
 function getTimestamp() {
   const date = new Date();
   return date.toISOString().replace(/:/g, "-");
+}
+
+export async function getWorkflowAsSVG(modeler) {
+  return modeler.saveSVG({ format: true });
 }
 
 export async function saveWorkflowAsSVG(modeler, fileName, fileFormat) {
